6ebeea230a253960f3fb8ec5644a2322a5e3d522
andrewjspivey <12spivey@gmail.com>
2021-05-03 13:34:44 -0700

    insert_employees working

--- insert_employees.js
+++ insert_employees.js
@@ -2,8 +2,6 @@ const axios = require('axios')
 const url = require('url')
 const sql = require('mssql'); 
 
-const subDays = require('date-fns/subDays')
-var format = require('date-fns/format')
 
 const dotenv = require('dotenv')
 dotenv.config()
@@ -12,17 +10,7 @@ const custId = process.env.ESO_CUST_ID
 const esoPassword = process.env.ESO_PASSWORD
 const vendorKey = process.env.ESO_VENDOR_KEY
 
-let startSubDaysAgo = subDays(new Date(), 4)
-let twoDaysAgo = format(new Date(startSubDaysAgo), 'MM/dd/yyyy')
-
-let endSubDaysAgo = subDays(new Date(), 3)
-let oneDayAgo = format(new Date(endSubDaysAgo), 'MM/dd/yyyy')
-
-console.log(twoDaysAgo)
-
-const esoUrl = `https://sched-api.esosuite.net/API_v1.7/EmployeeService.svc/GetPunches?custId=${custId}&pass=${esoPassword}&vendorKey=${vendorKey}`
-const params = new url.URLSearchParams({ starttime: twoDaysAgo, endtime: oneDayAgo});
-
+const esoUrl = `https://sched-api.esosuite.net/API_v1.7/EmployeeService.svc/GetEmployees?custId=${custId}&pass=${esoPassword}&vendorKey=${vendorKey}`
 
 const config = {
     user: process.env.DATABASE_USER,
@@ -33,31 +21,34 @@ const config = {
 };
 
 
-const pullAndInsertPunches = async () => {
+const pullAndInsertEmployees = async () => {
 
     try {
-        const response = await axios.get(esoUrl, {params, headers: {Accept: 'application/json'}})
+        const response = await axios.get(esoUrl, {headers: {Accept: 'application/json'}})
                 
-        var data = await response.data.Punches
-    
-        //console.log(data[15])
+        var data = await response.data.Employees
                 
         await sql.connect(config)
-
-        console.log("connected")
         
-        const table = new sql.Table("PunchesTableDockerTest");
+        const table = new sql.Table("EmployeesTest");
         table.create = true;
         
         table.columns.add('EmployeeId', sql.VarChar(20), { nullable: true });
         table.columns.add('FirstName', sql.VarChar(30), { nullable: true });
         table.columns.add('LastName', sql.VarChar(40), { nullable: true });
-        table.columns.add('InPunchTime', sql.VarChar(30), { nullable: true });
-        table.columns.add('OutPunchTime', sql.VarChar(30), { nullable: true });
-        table.columns.add('PunchId', sql.Int, { nullable: true });
-        table.columns.add('StartComment', sql.VarChar(sql.MAX), { nullable: true });
-        table.columns.add('EndComment', sql.VarChar(sql.MAX), { nullable: true });
-        table.columns.add('EHomeCostCenter', sql.VarChar(50), { nullable: true });
+        table.columns.add('Address', sql.VarChar(60), { nullable: true });
+        table.columns.add('BirthDate', sql.VarChar(40), { nullable: true });
+        table.columns.add('CellPhone', sql.VarChar(20), { nullable: true });
+        table.columns.add('City', sql.VarChar(30), { nullable: true });
+        table.columns.add('State', sql.VarChar(5), { nullable: true });
+        table.columns.add('Zip', sql.VarChar(12), { nullable: true });
+        table.columns.add('EmailAddress', sql.VarChar(40), { nullable: true });
+        table.columns.add('FTHireDate', sql.VarChar(30), { nullable: true });
+        table.columns.add('HomeCostCenter', sql.VarChar(10), { nullable: true });
+        table.columns.add('PayRate', sql.Decimal(4,2), { nullable: true });
+        table.columns.add('PayrollId', sql.VarChar(6), { nullable: true });
+        table.columns.add('Status', sql.Int, { nullable: true });
+        
         
         
 
@@ -66,8 +57,9 @@ const pullAndInsertPunches = async () => {
         
         for (i = 0; i < count; i++) {
         table.rows.add(data[i].EmployeeId, data[i].FirstName, data[i].LastName, 
-            data[i].InPunchTime, data[i].OutPunchTime, data[i].PunchId, data[i].StartComment, 
-            data[i].EndComment, data[i].eHomeCostCenter);
+            data[i].Address, data[i].BirthDate, data[i].CellPhone, data[i].City, 
+            data[i].State, data[i].Zip, data[i].EmailAddress, data[i].FTHireDate,
+            data[i].HomeCostCenter, data[i].PayRate, data[i].PayrollId, data[i].Status);
         }
         
         const req = new sql.Request();
@@ -92,4 +84,4 @@ const pullAndInsertPunches = async () => {
             
     }
     
-    pullAndInsertPunches()
+    pullAndInsertEmployees()
